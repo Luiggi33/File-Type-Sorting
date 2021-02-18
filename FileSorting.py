68c8ea3582a46193e17b84753cdcc8dc04d5b0cc
Luiggi33 <aron.rohr@web.de>
2021-02-18 21:57:27 +0100

    Added Script
    Scripts now there

---
+++ FileSorting.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+@author: luiggi
+"""
+
+import os 
+import shutil
+   
+# Select the Path you want to Sort and where you want to sort it
+path = 'C:/Users/luigg/Downloads'
+inputpath = 'C:/Users/luigg/Downloads'
+
+# List all Things in the input dir
+list_ = os.listdir(inputpath) 
+
+# go thru every file in the list
+for file_ in list_:
+    
+    # splicing to extract the type of the file
+    name, ext = os.path.splitext(file_) 
+  
+    ext = ext[1:] 
+    
+#   Skipping if invalid
+    if ext == '': 
+        continue
+    
+#   Skipping if the file to sort is a directory
+    if os.path.isdir(path + '/' + file_):
+        list_.remove(file_)
+        print( file_ + " didnt get moved because its a directory.")
+        continue        
+        
+#   if a folder already exists, move it
+    if os.path.exists(path+'/'+ext): 
+        shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
+        print(name + " got moved to " + path+'/'+ext+'/'+file_)
+    
+#   if a folder doesnt exists, create it and then move it
+    else: 
+        os.makedirs(path+'/'+ext) 
+        shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_) 
+        print(name + " got moved to " + path+'/'+ext+'/'+file_)
