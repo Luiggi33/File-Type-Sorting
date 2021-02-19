34ea7054be261894af039a5dc676ee2431d8d7c4
Luiggi33 <aron.rohr@web.de>
2021-02-19 08:27:18 +0100

    Cleanup

--- FileSorting.py
+++ FileSorting.py
@@ -1,43 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-@author: luiggi
+@author: luiggi33
 """
 
-import os 
+#importing os to get the files
+import os
+#importing shutil to move files
 import shutil
-   
-# Select the Path you want to Sort and where you want to sort it
-path = 'Path/To/Sort/It/To'
+
+# Select the Path you want to Sort it to
+outputpath = 'Path/To/Sort/It/To'
+# Select the Path from where to Sort
 inputpath = 'From/Where/To/Sort'
 
 # List all Things in the input dir
-list_ = os.listdir(inputpath) 
+list_ = os.listdir(inputpath)
 
 # go thru every file in the list
 for file_ in list_:
     
-    # splicing to extract the type of the file
-    name, ext = os.path.splitext(file_) 
-  
-    ext = ext[1:] 
-    
+#   splicing to extract the type of the file
+    name, ext = os.path.splitext(file_)
+
+    ext = ext[1:]
+
 #   Skipping if invalid
-    if ext == '': 
+    if ext == '':
         continue
     
 #   Skipping if the file to sort is a directory
-    if os.path.isdir(path + '/' + file_):
+    if os.path.isdir(outputpath + '/' + file_):
         list_.remove(file_)
         print( file_ + " didnt get moved because its a directory.")
-        continue        
+        continue
         
 #   if a folder already exists, move it
-    if os.path.exists(path+'/'+ext): 
-        shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
-        print(name + " got moved to " + path+'/'+ext+'/'+file_)
-    
+    if os.path.exists(outputpath+'/'+ext):
+        shutil.move(outputpath+'/'+file_, outputpath+'/'+ext+'/'+file_)
+        print(name + " got moved to " + outputpath+'/'+ext+'/'+file_)
+
 #   if a folder doesnt exists, create it and then move it
-    else: 
-        os.makedirs(path+'/'+ext) 
-        shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_) 
-        print(name + " got moved to " + path+'/'+ext+'/'+file_)
+    else:
+        os.makedirs(outputpath+'/'+ext)
+        shutil.move(outputpath+'/'+file_, outputpath+'/'+ext+'/'+file_)
+        print(name + " got moved to " + outputpath+'/'+ext+'/'+file_)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-File Type Sorting
+# File Type Sorting
+
+Setup:
+	1. Download the Python Files
+	2. Setup your Input and Output Folder as you like
+	3. Run the Python File
